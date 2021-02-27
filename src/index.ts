cda0747ea82ba57a8f6ec2fd46b0bfcc66c5ac46
ganesh.g31 <ganesh.g3105@gmail.com>
2021-02-27 07:31:11 +0530

    feat(API): add methods to GET, PUT and POST data
    Added feature to fetch/update existing record and post new record.
    Set up json-server to act as a backend db.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "myName", age: 28 });
 
-user.on('change', () => console.log("Change #1"))
-user.on('change', () => console.log("Change #2"))
-user.on('save', () => console.log("Save #1"))
+const user = new User({ name: "new record", age: 3 });
 
-user.trigger('change')
-user.trigger('save')
+user.save();
